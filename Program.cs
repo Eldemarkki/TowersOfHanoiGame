2b5354b8f127fe166e189082fe0c230c50d9b4fb
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2020-04-25 22:28:36 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace TowerOfHanoi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TowerOfHanoiGame game = new TowerOfHanoiGame(4);
+
+            Console.WriteLine("Welcome to a the 'Towers of Hanoi' game!");
+            Console.WriteLine("The goal is to move the disks from the first tower to any other tower. However, you can not move a big disk on top of a smaller disk, it has to be bigger than the disk below it.");
+            Console.WriteLine("You can move a disk by typing 'move <number> <number>'. The first number is the tower where you want to move the disk from, and the second number is the tower where you want to move the disk to.");
+
+            game.Print();
+            while (true)
+            {
+                Console.Write("> ");
+                string[] commandArgs = Console.ReadLine().Split(' ');
+                string command = commandArgs[0].ToLower().Trim();
+
+                if (command == "move")
+                {
+                    if (commandArgs.Length != 3)
+                    {
+                        Console.WriteLine("You need to provide 3 parameters!");
+                        continue;
+                    }
+
+                    int fromIndex = -1;
+                    if (!int.TryParse(commandArgs[1], out fromIndex))
+                    {
+                        Console.WriteLine("The first argument was not a number!");
+                        continue;
+                    }
+
+                    int toIndex = -1;
+                    if(!int.TryParse(commandArgs[2], out toIndex))
+                    {
+                        Console.WriteLine("The second argument was not a number!");
+                        continue;
+                    }
+
+                    if (fromIndex < 1 || fromIndex > 3)
+                    {
+                        Console.WriteLine("The first argument must be between 1 and 3!");
+                        continue;
+                    }
+
+                    if (toIndex < 1 || toIndex > 3)
+                    {
+                        Console.WriteLine("The second argument must be between 1 and 3!");
+                        continue;
+                    }
+
+                    if (!game.Move(fromIndex - 1, toIndex - 1))
+                    {
+                        Console.WriteLine("That's an invalid move!");
+                        continue;
+                    }
+
+                    bool isOver = game.IsOver();
+                    if (isOver)
+                    {
+                        Console.WriteLine("Game over! You moved all the disks correctly!");
+                        break;
+                    }
+                }
+                else if (command == "undo")
+                {
+                    if (!game.Undo())
+                    {
+                        Console.WriteLine("There was nothing to undo!");
+                    }
+                }
+                else if (command == "quit")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ TowerOfHanoiGame.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TowerOfHanoi
+{
+    public class TowerOfHanoiGame
+    {
+        private const int towerCount = 3;
+
+        private List<int>[] towers;
+        private int diskCount;
+
+        private Queue<(int, int)> moveHistory;
+
+        public TowerOfHanoiGame(int diskCount)
+        {
+            towers = new List<int>[towerCount];
+            moveHistory = new Queue<(int, int)>();
+
+            this.diskCount = diskCount;
+
+            towers[0] = Enumerable.Range(1, diskCount).Reverse().ToList();
+            for (int i = 1; i < towerCount; i++)
+            {
+                towers[i] = new List<int>(diskCount);
+            }
+        }
+
+        public bool Move(int sourceTowerIndex, int targetTowerIndex)
+        {
+            return Move(sourceTowerIndex, targetTowerIndex, true);
+        }
+
+        private bool Move(int sourceTowerIndex, int targetTowerIndex, bool addToHistory)
+        {
+            List<int> sourceTower = towers[sourceTowerIndex];
+            List<int> targetTower = towers[targetTowerIndex];
+
+            int sourceTowerTopMost = int.MaxValue;
+            if (sourceTower.Count != 0) sourceTowerTopMost = sourceTower[sourceTower.Count - 1]; // Top-most disk of source tower
+
+            int targetTowerTopMost = int.MaxValue;
+            if (targetTower.Count != 0) targetTowerTopMost = targetTower[targetTower.Count - 1]; // Top-most disk of target tower
+
+            if (sourceTowerTopMost == -1) return false;
+
+            if (sourceTowerTopMost < targetTowerTopMost)
+            {
+                if(addToHistory) moveHistory.Enqueue((sourceTowerIndex, targetTowerIndex));
+                
+                sourceTower.RemoveAt(sourceTower.Count - 1);
+                targetTower.Add(sourceTowerTopMost);
+                Print();
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool Undo()
+        {
+            if (moveHistory.Count == 0) return false;
+
+            (int, int) move = moveHistory.Dequeue();
+            Move(move.Item2, move.Item1, false);
+            return true;
+        }
+
+        public bool IsOver()
+        {
+            // Skip the first tower, and check if any of the other towers length is the disk count.
+            return towers.Skip(1).Any(l => l.Count == diskCount);
+        }
+
+        public List<int> GetTower(int towerIndex)
+        {
+            return towers[towerIndex];
+        }
+
+        public void Print()
+        {
+            int spacing = diskCount > 0 ? diskCount.ToString().Length : 0;
+            int maxHeight = Math.Max(Math.Max(towers[0].Count, towers[1].Count), Math.Max(towers[1].Count, towers[2].Count)); // This works only for 3 towers
+
+            Console.WriteLine("");
+
+            for (int i = maxHeight - 1; i >= 0; i--)
+            {
+                string line = "";
+                for (int j = 0; j < towerCount; j++)
+                {
+                    List<int> tower = towers[j];
+
+                    int value = 0;
+                    if (i < tower.Count) value = tower[i];
+
+                    line += ReplaceUntilDifferentCharacter(value.ToString("D" + spacing), '0', ' ') + " ";
+                }
+                Console.WriteLine(line);
+            }
+        }
+
+        private string ReplaceUntilDifferentCharacter(string text, char charToReplace, char replaceWith)
+        {
+            var result = text.ToCharArray();
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (result[i] == charToReplace)
+                {
+                    result[i] = replaceWith;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return new string(result);
+        }
+    }
+}
